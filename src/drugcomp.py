706af33b43426dd6630d3a5aef5dcad5fa35d955
domkirkham <dominickirkham@gmail.com>
2024-11-02 15:13:59 +0000

    Filter by clinical trial phase

--- src/drugcomp.py
+++ src/drugcomp.py
@@ -5,25 +5,27 @@ from chembl_webresource_client.new_client import new_client
 
 
 class Drug:
-    def __init__(self, smiles: str, name=None, chembl_id=None, max_phase=None, indication=None):
+    def __init__(self, smiles: str, name=None, chembl_id=None, max_phase=None, indication=None, first_approval=None):
         # Get drug info from ChEMBL
         self.name = name
         self.chembl_id = chembl_id
         self.smiles = smiles
         self.max_phase = max_phase
         self.indication = indication
-        self.similarity = {}
+        self.first_approval = first_approval
+        self.similarity = []
 
-    def get_similar_drugs(self) -> list:
+    def get_similar_drugs(self, clin_phase=4) -> list:
         similarity = new_client.similarity
-        res = similarity.filter(smiles=self.smiles, similarity=70).only(['molecule_chembl_id', 'pref_name', 'max_phase', 'first_approval', 'indication_class', 'molecule_structures__canonical_smiles', 'similarity'])
+        res = similarity.filter(smiles=self.smiles, similarity=40).filter(max_phase=clin_phase).only(['molecule_chembl_id', 'pref_name', 'max_phase', 'first_approval', 'indication_class', 'molecule_structures__canonical_smiles', 'similarity'])
 
         # Create similar drug objects and add ChEMBL info
         sim_drugs_list = [(Drug(drug["molecule_structures"]["canonical_smiles"],
                                 name=drug["pref_name"],
                                 chembl_id=drug["molecule_chembl_id"],
                                 max_phase=drug["max_phase"],
-                                indication=drug["indication_class"]), drug["similarity"]) for drug in res]
+                                indication=drug["indication_class"],
+                                first_approval=drug["first_approval"]), drug["similarity"]) for drug in res]
         
         self.similarity = [(drug["molecule_chembl_id"], drug["similarity"]) for drug in res]
 
@@ -42,5 +44,7 @@ class Drug:
         cosine_sim = cosine_similarity(mol1_tfidf, mol2_tfidf)
 
         return cosine_sim
+    
+
 
 
